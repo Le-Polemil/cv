ca19816e37b6f8f40d38bba6e7c5baca5c23f6e1
Cobelt <paul_emile@hotmail.fr>
2024-08-27 23:18:20 +0200

    fix: import order due to prettier new conf

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,28 @@
-import React from "react"
 import { ApolloProvider } from "@apollo/client"
-import ReactDOM from "react-dom/client"
+import * as dayjs from "dayjs"
+import "dayjs/locale/en" // import locale
+import "dayjs/locale/fr" // import locale
+import "dayjs/locale/ru" // import locale
 import i18n from "i18next"
 import Backend from "i18next-http-backend"
+import React from "react"
+import ReactDOM from "react-dom/client"
 import { initReactI18next } from "react-i18next"
-import * as dayjs from "dayjs"
-import "dayjs/locale/fr" // import locale
-import "dayjs/locale/en" // import locale
-import "dayjs/locale/ru" // import locale
 
-import { TabContextProvider } from "./contexts/TabContext.tsx"
-import { API_BASE_URL } from "./constants/index.ts"
 import client from "./apollo.ts"
 import App from "./App.tsx"
+import { API_BASE_URL } from "./constants"
+import { TabContextProvider } from "./contexts/TabContext.tsx"
 import "./index.css"
 
-import "@fontsource/lato"
-import "@fontsource/lato/700.css"
-import "@fontsource/rubik"
-import "@fontsource/rubik/600.css"
 import "@fontsource/jost"
 import "@fontsource/jost/600.css"
 import "@fontsource/jost/700.css"
+import "@fontsource/lato"
+import "@fontsource/lato/700.css"
 import "@fontsource/material-icons"
+import "@fontsource/rubik"
+import "@fontsource/rubik/600.css"
 
 dayjs.locale("fr")
 
